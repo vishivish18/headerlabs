4e3708bf6d8405e7a268e56b5a428b7a1cdc0258
Vishal <vishivish18@gmail.com>
2015-05-03 20:21:22 +0530

    Added file size response on client\

--- app.js
+++ app.js
@@ -1,43 +1,58 @@
-var express    =       require("express");
 var multer     =       require('multer');
-var app        =       express();
 var done       =       false;
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require("socket.io")(http);
+
+
+var socket = io.listen(1222);  
+
+io.on('connection',function(socket){
+    console.log('hellll0 from server');
+    
+    app.post('/api/photo',function(req,res){
+      if(done==true){
+        console.log(req.files);
+        console.log('this is the size'+ (req.files['userPhoto'].size/1024)+'kb');
+        var filesize = (req.files['userPhoto'].size/1024);        
+        console.log('File size is '+filesize);
+        
+        res.end("File uploaded.");
+      }
+    
+    var testing = "just a test";
+        socket.emit('test', {'test': req.files['userPhoto'].size});    
+});
+
+        
+    
+});
+
+
+
 
 app.use(multer({ dest: './uploads/',
  rename: function (fieldname, filename) {
     return filename+Date.now();
   },
 onFileUploadStart: function (file) {
-  console.log(file.originalname + ' is starting ...'+file.length)
-for (i = 0; i < file.length; i++) {
-    console.log('uploadig');
-}
-
+  console.log(file.originalname + ' is starting ...')
 },
 onFileUploadComplete: function (file) {
   console.log(file.fieldname + ' uploaded to  ' + file.path)
   done=true;
+      
 }
 }));
 
-app.use(express.static(__dirname + '/public'));
+
 
 app.get('/',function(req,res){
       res.sendfile("index.html");
 });
 
-app.post('/api/photo',function(req,res){
 
-if(done==true){
-    console.log(req.files);
-    console.log('this is the size'+ (req.files['userPhoto'].size/1024)+'kb');
-      
-      
-    res.end("File uploaded.");
-  }
-   
-});
 
-app.listen(3000,function(){
-    console.log("listening on port 3000");
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
